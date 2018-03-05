16ee077c12ac78098694b37ececd92ab92bce495
MT_CENT <mtc9417@gmail.com>
2018-03-05 11:53:46 -0500

    Menu System
    I've added a simple system so we can navigate between the scenes while playing the game.

---
+++ The Fate of Montoya/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void StartGameBtn(string Main){
+		SceneManager.LoadScene ("Main");
+	}
+	public void GameoverScreen(string GameOver){
+		SceneManager.LoadScene ("GameOver");
+	}
+	public void VictoryScreen(string Victory){
+		SceneManager.LoadScene ("Victory");
+	}
+	public void MainMenuBtn(string StartMenu){
+		SceneManager.LoadScene ("StartMenu");
+		GameObject [] objects = GameObject.FindObjectsOfType <GameObject>();
+		foreach (GameObject o in objects) {
+			Destroy (o);
+		}
+	}
+}
+

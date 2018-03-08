6298f2936590079e3300bb01a6b4579c6f4c55a5
MT_CENT <mtc9417@gmail.com>
2018-03-07 20:54:26 -0500

    added white box map

--- The Fate of Montoya/Assets/Scripts/LevelManager.cs
+++ The Fate of Montoya/Assets/Scripts/LevelManager.cs
@@ -5,15 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	//Button to go to Main Game Scene
 	public void StartGameBtn(string Main){
 		SceneManager.LoadScene ("Main");
 	}
+
+	//Button to go to Defeat Game Scene
 	public void GameoverScreen(string GameOver){
 		SceneManager.LoadScene ("GameOver");
 	}
+
+	//Button to go to Victory Game Scene
 	public void VictoryScreen(string Victory){
 		SceneManager.LoadScene ("Victory");
 	}
+
+	//Button to go to the Main Menu (will also destroy all objects in game)
 	public void MainMenuBtn(string StartMenu){
 		SceneManager.LoadScene ("StartMenu");
 		GameObject [] objects = GameObject.FindObjectsOfType <GameObject>();
